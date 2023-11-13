15d8eadbdc8dbf08e89258efd546f9bac32d039e
Gopal Dnyaneshwar Jogi <gopaljogi5@gmail.com>
2023-11-13 18:20:10 +0530

    Changes to be committed: 	new file:   css/style.css 	new file:   img/my1.png 	new file:   img/resume.pdf 	new file:   index.html 	new file:   js/main.js

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// active menu
+let menuIcon = document.querySelector(".menu-icon");
+let navlist = document.querySelector(".navlist");
+menuIcon.addEventListener("click",()=>{
+    menuIcon.classList.toggle("active");
+    navlist.classList.toggle("active");
+    document.body.classList.toggle("open");
+});
+
+// remove navlist
+navlist.addEventListener("click",()=>{
+    navlist.classList.remove("active");
+    menuIcon.classList.remove("active"); 
+    document.body.classList.remove("open");   
+});
+
+// active menu
+let menuLi = document.querySelectorAll("header ul li a");
+let section = document.querySelectorAll("section");
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 <section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+activeMenu()
+window.addEventListener("scroll",activeMenu);
+
+// text animation
+let text1 = document.querySelector(".second-text");
+
+let textloade = () => {
+    setTimeout(() =>{
+        text1.textContent= "Python Developer";
+    },0);
+    setTimeout(() =>{
+        text1.textContent= "Web Developer";
+    },4000);
+    setTimeout(() =>{
+        text1.textContent= "DevOps Engineer";
+    },8000);
+}
+    textloade();
+    setInterval(textloade,12000);
+
+// rotate text js code 
+let text = document.querySelector(".text p");
+
+text.innerHTML = text.innerHTML.split("").map((char,i)=>
+    `<b style="transform:rotate(${i * 6.3}deg")>${char}</b>`
+).join("");
