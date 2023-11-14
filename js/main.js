a6cdfe5720b670d16530b0068cd0bcd3951baf7d
Gopal Dnyaneshwar Jogi <gopaljogi5@gmail.com>
2023-11-14 17:16:06 +0530

    Changes to be committed: 	new file:   ab.txt 	modified:   css/style.css 	new file:   img/about.png 	new file:   img/shapes/Black_and_Red_Circle.png 	new file:   img/shapes/circle.png 	new file:   img/shapes/half-ring.png 	new file:   img/shapes/my.png 	new file:   img/shapes/ring.png 	new file:   img/shapes/second-circle.png 	modified:   index.html 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -50,3 +50,17 @@ let text = document.querySelector(".text p");
 text.innerHTML = text.innerHTML.split("").map((char,i)=>
     `<b style="transform:rotate(${i * 6.3}deg")>${char}</b>`
 ).join("");
+
+// about switch botton
+
+const buttons = document.querySelectorAll(".about-btn button");
+const contents = document.querySelectorAll(".content");
+
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        contents.forEach(content => content.style.display ='none');
+        contents[index].style.display = 'block';
+        buttons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+    });
+});
