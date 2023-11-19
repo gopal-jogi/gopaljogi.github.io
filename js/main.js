0360a4c39a95b56e3e28d8ce860629625ebdd5c5
Gopal Dnyaneshwar Jogi <gopaljogi5@gmail.com>
2023-11-19 22:36:05 +0530

    Changes to be committed: 	modified:   css/style.css 	modified:   index.html 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -109,3 +109,95 @@ let tl = gsap.timeline({
     x:"-1112",
   },'pair2');
   
+
+const first_skill = document.querySelector(".skills:first-child");
+const sk_counters = document.querySelectorAll(".counter span");
+const progress_bars = document.querySelectorAll(".skillsProgres svg circle");
+
+window.addEventListener("scroll",()=>{
+    if(!skillsPlayed)
+    skillsCounter();
+})
+
+
+function hasReached(el){
+    let topPosition = el.getBoundingClientRect().top;
+    if(window.innerHeight >= topPosition + el.offsetHeight)return true;
+    return false;
+}
+
+function updateCount(num,maxNum){
+    let currentNum = +num.innerText;
+    
+    if(currentNum < maxNum){
+        num.innerText = currentNum + 1;
+        setTimeout(()=>{
+            updateCount(num,maxNum)
+        },12)
+    }
+}
+
+
+let skillsPlayed = false;
+
+function skillsCounter(){
+    if(!hasReached(first_skill))return;
+    skillsPlayed = true;
+    sk_counters.forEach((counter,i)=>{
+        let target = +counter.dataset.target;
+        let strokeValue = 465 - 465 * (target / 100);
+
+        progress_bars[i].style.setProperty("--target",strokeValue);
+
+        setTimeout(()=>{
+            updateCount(counter,target);
+        },400)
+    });
+
+    progress_bars.forEach(p => p.style.animation = "progress 2s ease-in-out forwards");
+}
+
+
+// side progress bar 
+
+let calcScrollValue = ()=>{
+    let scrollProgress = document.getElementById("progress");
+    let pos = document.documentElement.scrollTop;
+
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100)/calcHeight);
+    
+    if(pos > 100){
+        scrollProgress.style.display = "grid";
+    }else{
+        scrollProgress.style.display = "none";
+    }
+
+    scrollProgress.addEventListener("click",()=>{
+        document.documentElement.scrollTop = 0;
+    });
+
+    scrollProgress.style.background = `conic-gradient(#000 ${scrollValue}%,#e6006d ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
+
+
+
+// scroll reveal
+
+ScrollReveal({ 
+    distance:"90px",
+    duration:2000,
+    delay:200,
+    // reset: true ,
+});
+
+
+ScrollReveal().reveal('.my-info,.main-text,.proposal,.heading', { origin: "top" });
+ScrollReveal().reveal('.about-img,.fillter-buttons,.contact-info', { origin: "left" });
+ScrollReveal().reveal('.about-content,.skillsProgres', { origin: "right" });
+ScrollReveal().reveal('.allServices,.portfolio-gallery,footer,.img-my', { origin: "bottom" });
+
